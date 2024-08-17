d44133b906411a1d955fe50931bede7bf382cbe2
jetyamato <joemtamayo@protonmail.com>
2024-08-17 09:11:19 +0800

    Fetch Products by Search Query

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import "./Navbar.css";
 import Rocket from "../../assets/rocket.png";
@@ -8,23 +8,40 @@ import Memo from "../../assets/memo.png";
 import Order from "../../assets/package.png";
 import Lock from "../../assets/locked.png";
 import LinkWithIcon from "./LinkWithIcon";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import UserContext from "../../contexts/UserContext";
 import CartContext from "../../contexts/CartContext";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
   const user = useContext(UserContext);
   const { cart } = useContext(CartContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (search.trim() !== "") {
+      navigate(`/products?search=${search.trim()}`);
+    }
+  };
 
   return (
     <nav className="align_center navbar">
       <div className="align_center">
         <h1 className="navbar_heading">Digital Candy</h1>
-        <form action="" className="align_center navbar_form">
+        <form
+          action=""
+          className="align_center navbar_form"
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             className="navbar_search"
             placeholder="Search Products"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <button type="submit" className="search_button">
             Search

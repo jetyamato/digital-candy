615b7542b3a66580edade6b595a23934dce90500
jetyamato <joemtamayo@protonmail.com>
2024-08-17 08:59:08 +0800

    Fetch Featured Products

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -12,14 +12,14 @@ const HomePage = () => {
       <HeroSection
         title="Buy iPhone 14 Pro"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
-        link="/"
+        link="/product/66bff2ad9622ae44a1331052"
         image={Iphone}
       />
       <FeaturedProducts />
       <HeroSection
         title="Build the Ultimate Setup"
         subtitle="You can add Studio Display and color-matched Magic Accessories to your bag after you configure your Mac Mini."
-        link="/"
+        link="/product/66bff2ad9622ae44a133105a"
         image={Mac}
       />
     </div>
